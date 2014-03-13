03151c40166bad14ee3995836765f06d14f7e40a
Hugo Bastien <hugobast@gmail.com>
2014-03-13 13:28:30 -0400

    Proof of concept nfbsearch with vanilla python and flask

---
+++ nfbsearch_flask/nfbsearch_flask.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from elasticsearch import Elasticsearch
+
+app = Flask(__name__)
+es = Elasticsearch(["vmdev.nfb.ca:9200"])
+
+
+@app.route("/search")
+def search():
+    query = request.args.get('q', None)
+    if not query:
+        return "please provide a q parameter"
+
+    return jsonify(**es.search(index="nfb_films", doc_type="films", body={
+        "query": {
+            "bool": {
+                "should": {
+                    "query_string": {
+                        "default_field": "_all",
+                        "query": query
+                    }
+                }
+            }
+        }
+    }))
+
+app.run(debug=True)
